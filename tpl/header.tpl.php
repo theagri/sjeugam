4ce0f677f5bc825a3300e3ebc411c38d413f30d9
kallepersson <hi@kallepersson.se>
2012-01-14 16:12:46 +0100

    Added <link> element for RSS feed (thanks jnordberg\!)

--- tpl/header.tpl.php
+++ tpl/header.tpl.php
@@ -7,6 +7,7 @@
 		<base href="<?=SJEUGAM_BASE_URL?>"></base>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<link rel="stylesheet" type="text/css" href="assets/style.css">
+		<link rel="alternate" type="application/rss+xml" href="./<?=SJEUGAM_USE_REWRITE ? '' :'?route=' ?>feed">
 	</head>
 	<body>
 	<header>
